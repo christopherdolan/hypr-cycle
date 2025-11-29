43bc4b097488a9b23f1b0926f4c598bf544ae834
Chris Dolan <aelien27@gmail.com>
2025-11-29 15:40:12 +0530

    Improved readability. Added more assertions to tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,31 @@
 pub mod connection;
 pub mod domain;
 
+use std::str::FromStr;
+use clap::{ValueEnum};
+
 use domain::{OwnedMonitor,OwnedWorkspace};
 use connection::HyprlandClient;
+
+#[derive(Debug, Clone, ValueEnum)]
+pub enum Direction {
+    Next,
+    Previous,
+}
+
+impl FromStr for Direction {
+    type Err  = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim().to_ascii_lowercase();
+
+        match s.as_str() {
+            "next" => Ok(Direction::Next),
+            x if ["prev","previous"].contains(&x) => Ok(Direction::Previous),
+            _ => Err("Unrecognized direction"),
+        }
+    }
+}
 
 pub fn get_focused_monitor(conn: &mut dyn HyprlandClient)
     -> anyhow::Result<OwnedMonitor> {
@@ -15,6 +38,9 @@ pub fn get_focused_monitor(conn: &mut dyn HyprlandClient)
     Ok(monitor.clone())
 }
 
+/// Returns a sorted list of the workspaces bound to the provided monitor.
+/// Throws an error if the provided monitor doesn't have any workspaces
+/// bound to it.
 pub fn get_workspaces_for_monitor(
     conn: &mut dyn HyprlandClient,
     monitor: &OwnedMonitor
@@ -46,6 +72,26 @@ pub fn get_current_workspace(
     Ok(active_workspace)
 }
 
+/// The index of the sorted list of workspaces tells us where to
+/// target the upcoming workspace switch.
+pub fn get_target_workspace(
+    all_workspaces: Vec<OwnedWorkspace>,
+    active_workspace: OwnedWorkspace,
+    direction: Direction)
+-> anyhow::Result<OwnedWorkspace> {
+    let idx = all_workspaces
+        .iter()
+        .position(|w| w == &active_workspace)
+        .ok_or_else(|| anyhow::anyhow!("Current workspace not found"))?;
+    let len = all_workspaces.len();
+
+    let next_idx = match direction {
+        Direction::Next => (idx + 1) % len,
+        Direction::Previous => (idx + len - 1) % len,
+    };
+    Ok(all_workspaces[next_idx].clone())
+}
+
 pub fn switch_to_workspace(
     conn: &mut dyn HyprlandClient,
     target: &OwnedWorkspace
@@ -62,29 +108,21 @@ pub mod fixtures {
         OwnedWorkspace::new(id, mon.to_string())
     }
 
-    pub fn mon(
-        name: &str,
-        id: i64,
-        focused: bool,
-        active_id: i64
+    pub fn mon(name: &str, id: i64, focused: bool, active_id: i64
     ) -> OwnedMonitor {
-        OwnedMonitor::new(
-            name.to_string(),
-            id,
-            focused,
-            ws(active_id, name),
-        )
+        OwnedMonitor::new(name.to_string(), id, focused, ws(active_id, name))
     }
 
     pub fn monitors() -> Vec<OwnedMonitor> {
         vec!(
-            mon("eDP-1", 1, true, 1),
+            mon("eDP-1", 1, true, 1), //active monitor
             mon("HDMI-1", 2, false, 3),
         )
     }
 
     pub fn workspaces() -> Vec<OwnedWorkspace> {
         vec!(
+            ws(-97, "eDP-1"), //hidden workspace ("scratch")
             ws(1, "eDP-1"),
             ws(2, "eDP-1"),
             ws(3, "HDMI-1")
@@ -104,6 +142,8 @@ mod tests {
             .returning(move || Ok(fixtures::workspaces()));
     }
 
+    /// There are two monitors in the fixture. One is marked active.
+    /// This test ensures that the active monitor is returned by the function.
     #[test]
     fn test_get_focused_monitor() {
         let conn = &mut MockHyprlandClient::new();
@@ -111,24 +151,43 @@ mod tests {
 
         assert_eq!(
             get_focused_monitor(conn).unwrap(),
-            fixtures::monitors()[0]
+            fixtures::monitors()
+                .iter()
+                .find(|m| m.focused())
+                .unwrap()
+                .clone()
         );
     }
 
+    /// The first monitor has three workspaces, but only two are visible.
+    /// This test ensures that only the visible workspaces are returned
+    /// by the function.
     #[test]
     fn test_get_workspaces_for_monitor() {
         let conn = &mut MockHyprlandClient::new();
         create_mock(conn);
 
-        assert!(get_workspaces_for_monitor(
-            conn,
-            &fixtures::monitors()[0])
-                .unwrap()
-                .iter()
-                .any(|m| m == &fixtures::workspaces()[0])
-        );
+        let target_monitor = &fixtures::monitors()[0];
+        let workspaces = get_workspaces_for_monitor(conn, target_monitor)
+            .unwrap();
+
+        // None of the returned workspaces is invisible
+        assert_eq!(None, workspaces.iter().find(|w| w.id() < 0));
+
+        let expected_workspaces: Vec<OwnedWorkspace> = 
+            fixtures::workspaces().into_iter()
+                .filter(|w|
+                    w.id() > 0
+                    && w.monitor_name() == target_monitor.name()
+                ).collect();
+       
+        // All of the expected workspaces are visible
+        assert!(expected_workspaces.iter().all(|w| workspaces.contains(w)));
     }
 
+    /// Monitors each keep track of their active workspace.
+    /// Of the two monitors in the fixture, one is marked focused.
+    /// This test ensures that the function returns the focused monitor's active workspace.
     #[test]
     fn test_get_current_workspace() {
         let conn = &mut MockHyprlandClient::new();
@@ -136,10 +195,16 @@ mod tests {
 
         assert_eq!(
             get_current_workspace(conn).unwrap().id(),
-            fixtures::workspaces()[0].id()
+            fixtures::monitors()
+                .iter()
+                .find(|m| m.focused())
+                .unwrap()
+                    .active_workspace().id()
         );
     }
 
+    /// Hard to test this function's behavior. We can only really ensure that the right underlying
+    /// function call is made.
     #[test]
     fn test_switch_to_workspace() {
         let conn = &mut MockHyprlandClient::new();
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,27 @@
-use crate::connection::RealHyprlandClient;
-use hypr_cycle::*;
-
+// TODO: Have RealHyprlandClient build the connection itself
 use clap::Parser;
 use hyprrust::HyprlandConnection;
 
+use hypr_cycle::*;
+use connection::RealHyprlandClient;
+
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-struct Args {
+pub struct Args {
     #[arg(default_value = "next")]
-    /// Direction to switch workspace ('next' or 'prev')
-    direction: String,
+    /// Direction to switch workspace ('next' or 'prev[ious]')
+    direction: Direction,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+    let direction = args.direction;
 
     let conn = &mut RealHyprlandClient::new(HyprlandConnection::current()?);
-
     let monitor = get_focused_monitor(conn)?;
-    let workspaces = get_workspaces_for_monitor(
-        conn,
-        &monitor
-    )?;
+    let workspaces = get_workspaces_for_monitor(conn,&monitor)?;
     let current_ws = get_current_workspace(conn)?;
-    let idx = workspaces
-        .iter()
-        .position(|w| w == &current_ws)
-        .ok_or_else(|| anyhow::anyhow!("Current workspace not found"))?;
 
-    let next_idx = if args.direction == "next" {
-        (idx + 1) % workspaces.len()
-    } else if args.direction == "prev" {
-        (idx + workspaces.len() - 1) % workspaces.len()
-    } else {
-        return Err(anyhow::anyhow!("Direction must be 'next' or 'prev'"));
-    };
-
-    let target = workspaces[next_idx].clone();
-    switch_to_workspace(conn, &target)?;
-    Ok(())
+    let target = get_target_workspace(workspaces, current_ws, direction)?;
+    switch_to_workspace(conn, &target)
 }
